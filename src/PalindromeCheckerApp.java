e080ceb98d3590d7507bc89b84b4881e3edab291
IPSITA ROY <ipshitaroy2007@gmail.com>
2026-03-09 19:10:49 +0530

    UC4

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,27 @@
-import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
-        String original = scanner.nextLine();
+        String input = "level";
 
-        String reversed = "";
+        char[] characters = input.toCharArray();
 
-        // Reverse the string using for loop
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i); // String Concatenation
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if (original.equals(reversed)) {
-            System.out.println("The string is a Palindrome.");
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
         } else {
-            System.out.println("The string is NOT a Palindrome.");
+            System.out.println(input + " is NOT a Palindrome.");
         }
-        scanner.close();
     }
 }
